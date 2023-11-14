6dfd12778cf67263624786195ad97f51ff0ec919
OlofLindber9 <134716383+OlofLindber9@users.noreply.github.com>
2023-11-14 17:31:46 +0100

    Update Animator.java

--- bouncing_balls/Animator.java
+++ bouncing_balls/Animator.java
@@ -88,8 +88,6 @@ public final class Animator extends JPanel implements ActionListener {
     }
 
 	public static void main(String[] args) {
-        // Schedule a job for the event-dispatching thread:
-        // creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 Animator anim = new Animator(1920, 1080, 120);
